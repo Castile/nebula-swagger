61a3855ccd7a50581a9fbb01ff50103e6c2813e2
fulin <fulin@hoteamsoft.com>
2023-04-10 14:56:29 +0800

    ✌ 项目在线文档升级为knife4j 🌴 md文件整理升级

--- src/main/java/com/example/nebula/NebulaApplication.java
+++ src/main/java/com/example/nebula/NebulaApplication.java
@@ -1,18 +1,34 @@
 package com.example.nebula;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableSwagger2
-@EnableWebMvc
+@Slf4j
 public class NebulaApplication {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        SpringApplication.run(NebulaApplication.class, args);
+        SpringApplication app = new SpringApplication(NebulaApplication.class);
+        ConfigurableApplicationContext application = app.run(args);
+        Environment env = application.getEnvironment();
+        final String contextPath = env.getProperty("server.servlet.context-path");
+        log.info("\r\n----------------------------------------------------------" +
+                        "\r\nApplication is running! Access URLs:" +
+                        "\r\ntDoc: http://{}:{}{}/doc.html " +
+                        "\r\n----------------------------------------------------------",
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                contextPath, InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                contextPath);
     }
 
 }
--- src/main/java/com/example/nebula/config/SwaggerConfig.java
+++ src/main/java/com/example/nebula/config/SwaggerConfig.java
@@ -7,22 +7,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * swagger配置
+ *
  * @author fulin
  */
 @Configuration
-@EnableOpenApi
+@EnableSwagger2WebMvc
 public class SwaggerConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Bean(value = "defaultApi2")
     public Docket createRestApi() {
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo()).select()
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
@@ -31,6 +33,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
+                .description("图谱项目接口文档")
+                .contact(new Contact("fulin", "http://xxxx", "1792939443@qq.com"))
+                .version("v1.1.0")
+                .title("图谱项目 API")
                 .build();
     }
 
